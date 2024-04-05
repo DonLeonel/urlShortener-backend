6a5e222829e323d09c126f88fb92b74ef530c8a6
DonLeonel <leonel.don@hotmail.es>
2024-04-05 08:55:59 -0300

    add var env

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ const bodyParser = require('body-parser')
 app.use(cors());
 app.use(bodyParser.json());
 
-
 //Requerimos la ruta.
 const urlRouter = require('./routes/urlRouter');
 
@@ -15,4 +14,4 @@ const urlRouter = require('./routes/urlRouter');
 app.use(urlRouter);
 
 const _PORT = process.env.PORT || 3000;
-app.listen(_PORT, () => console.log(`Servidor corriendo en: http://localhost:${_PORT}`));
+app.listen(_PORT);
