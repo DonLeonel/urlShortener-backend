2a9c7f40920bc83555564de7fdfade6c68c67089
DonLeonel <leonel.don@hotmail.es>
2024-04-10 11:18:06 -0300

    config env

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
+    "username": 'root' || process.env.DB_USER,
+    "password": null || process.env.DB_PASSWORD,
+    "database": 'urlShortener' || process.env.DB_NAME,
+    "host": 'localhost' || process.env.DB_HOST,
     "dialect": "mysql"
   },
   "test": {
