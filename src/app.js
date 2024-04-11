505167d3c12f0c47755a079b257cb0d050d78186
DonLeonel <leonel.don@hotmail.es>
2024-04-11 06:47:18 -0300

    refact app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser')
 
-//app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -14,4 +14,4 @@ const urlRouter = require('./routes/urlRouter');
 app.use(urlRouter);
 
 const _PORT = process.env.PORT || 3000;
-app.listen(_PORT ,() => {console.log(_PORT);});
+app.listen(_PORT);
